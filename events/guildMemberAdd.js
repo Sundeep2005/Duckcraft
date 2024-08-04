bbcd69cf188d37f1fc21c1fb177fed3dae1f34d7
Sundeep <sundeep.dejongh@hotmail.com>
2024-08-04 17:24:34 +0200

    GuildMemberAdd Event

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,27 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+    name: Events.GuildMemberAdd,
+
+    async execute(member) {
+        const client = member.client;
+
+        const channel = member.guild.channels.cache.find(ch => ch.name === "👋┃binnenkomst");
+        if (!channel) return console.log("Welkom channel niet gevonden.");
+
+        const embed = new EmbedBuilder()
+            .setTitle('🎉 Welkom op de Duckcraft discord server!')
+            .setDescription(`Hey ${member}! 👋\nJe bent member **#${member.guild.memberCount}**. Kijk gerust eventjes rond!\n`)
+            .addFields(
+                { name: '📅 Gejoined op:', value: `${new Date().toLocaleDateString()}`, inline: true },
+                { name: '✏️ Account gemaakt op:', value: `${member.user.createdAt.toLocaleDateString()}`, inline: true }
+            )
+            .setColor(config.embedColor)
+            .setThumbnail(member.user.displayAvatarURL())
+            .setFooter({ text: `Welkom, ${member.user.username}!`, iconURL: member.user.displayAvatarURL() })
+            .setTimestamp();
+
+        await channel.send({ embeds: [embed] });
+    }
+}
--- utils/eventsHander.js
+++ utils/eventsHander.js
@@ -1,29 +1,22 @@
 const fs = require('node:fs');
+const path = require('node:path');
 
 async function loadEvents(client) {
-    const folders = fs.readdirSync('./events');
-    for (const folder of folders) {
-        const files = fs.readdirSync(`./events/${folder}`).filter(file => file.endsWith('.js'));
-        for (const file of files) {
-            const event = require(`../events/${folder}/${file}`);
-            if (event.rest) {
-                if (event.once) {
-                    client.once(event.name, (...args) => event.execute(...args, client));
-                } else {
-                    client.on(event.name, (...args) => event.execute(...args, client));
-                }
-            } else {
-                if (event.once) {
-                    client.once(event.name, event.execute);
-                } else {
-                    client.on(event.name, event.execute);
-                }
-            }
-            continue;
+
+    const eventsPath = path.join(__dirname, '../events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
         }
     }
 
-    return console.log('[EVENTS] ' + folders.length + ' Events loaded!');
+    return console.log('[EVENTS] ' + eventFiles.length + ' Events loaded!');
 }
 
 function getDirectories(path){
